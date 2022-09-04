164ea9c78491dda9fab3464fdab8951b752a2f00
forgottensemicolon #Muhammetcanpolat <73015593+mmc6185@users.noreply.github.com>
2022-09-04 21:33:12 +0300

    Create upload.sh

---
+++ stratos/stratos-sds/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true;
+do /usr/bin/head -c 25M /dev/urandom > "$HOME/upload/test-$(date '+%Y%m%d%H%M')" ; ppd terminal exec put "$HOME/upload/test-$(date '+%Y%m%d%H%M')";
+sleep 900;
+/usr/bin/rm -rf "$HOME/upload/test*";
+sleep 1;
+done
